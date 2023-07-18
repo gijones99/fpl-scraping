8d5a49876d6e0e08d237cbf401285937fac50941
Jones <joneslimwork@gmail.com>
2023-07-18 17:31:11 +0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,4 @@
+FBREF_HOMEPAGE_URL = "https://fbref.com/"
+PREM_URL = "https://fbref.com/en/comps/9/Premier-League-Stats"
+LEAGUE_TEAM_TABLE_ID = "results2022-202391_overall"
+PREM_LEAGUE_SCORES_URL = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
