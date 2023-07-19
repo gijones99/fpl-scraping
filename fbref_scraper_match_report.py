ce6a1e65b9dd7672a55032a03c708c2794614efa
Jones <joneslimwork@gmail.com>
2023-07-19 13:54:32 +0800

    Removed unnecessary imports for scraper_match_report.py

--- fbref_scraper_match_report.py
+++ fbref_scraper_match_report.py
@@ -1,12 +1,8 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 def load_scorebox_soup(url, driver, tag_id = "scorebox"):
     # Load the page using Selenium
